84284989f2f5db89aca9a67f39689809731163a3
Zach Neibaur <zneibaur@protonmail.com>
2023-03-03 12:00:28 -0700

    Adding a teams controller

---
+++ controllers/teams.ts
@@ -0,0 +1,86 @@
+import { MongoClient, ObjectId } from "https://deno.land/x/atlas_sdk@v1.1.0/mod.ts";
+const data_api_key = Deno.env.get("DATA_API_KEY");
+
+if (!data_api_key) throw new Error('API Key not found');
+
+const client = new MongoClient({
+  endpoint: "https://data.mongodb-api.com/app/data-yowii/endpoint/data/v1",
+  dataSource: "player-tracker-api",
+  auth: {
+    apiKey: data_api_key
+  }
+});
+
+const db = client.database("team_db");
+const teams = db.collection("teams");
+
+const addTeam = async ({request, response}: { request: any; response: any; }) => {
+  try {
+    if (!request.hasBody) {
+      response.status = 400;
+      response.body = {
+        success: false,
+        msg: "No Data"
+      }
+    } else {
+      const body = await request.body();
+      const player = await body.value;
+      const insertedId = await teams.insertOne({
+        _id: new ObjectId(),
+        data: player
+      });
+
+      response.status = 201;
+      response.body = {
+        success: true,
+        data: player,
+        insertedId
+      }
+    }
+  } catch (error) {
+    response.body = {
+      success: false,
+      msg: error.toString(),
+    }
+  }
+}
+
+// const getPlayers = async ({ response }: { response: any }) => {
+//   try {
+//     const allPlayers = await players.find();
+//     if (allPlayers) {
+//       response.status = 200;
+//       response.body = {
+//         success: true,
+//         data: allPlayers
+//       }
+//     }
+//   } catch (err) {
+//     response.body = {
+//       success: false,
+//       msg: err.toString()
+//     };
+//   }
+// };
+
+// const getPlayer = async ({ params, response }: { params: { id: string }; response: any }) => {
+//   try {
+//     const player = await players.findOne({
+//       id: parseInt(params.id),
+//     });
+//     if (player) {
+//       response.status = 200;
+//       response.body = {
+//         success: true,
+//         data: player
+//       }
+//     }
+//   } catch (error) {
+//    response.body = {
+//     success: false,
+//     msg: error.toString()
+//    } 
+//   }
+// }
+
+export { addTeam };
