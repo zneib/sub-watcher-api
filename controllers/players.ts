e323ee555644256a22f10a8f29bd0dcac38162d4
Zach Neibaur <zneibaur@protonmail.com>
2023-03-03 07:12:28 -0700

    Removing old code and attempting a new add player route

--- controllers/players.ts
+++ controllers/players.ts
@@ -1,18 +1,5 @@
-import { MongoClient } from "https://deno.land/x/atlas_sdk@v1.1.0/mod.ts";
+import { MongoClient, ObjectId } from "https://deno.land/x/atlas_sdk@v1.1.0/mod.ts";
 const data_api_key = Deno.env.get("DATA_API_KEY");
-// const BASE_URI = `https://data.mongodb-api.com/app/data-yowii/endpoint/data/beta/action`;
-// const DATA_SOURCE = "player-tracker-api";
-// const DATABASE = "player_db";
-// const COLLECTION = "players";
-
-// const options = {
-//   method: "POST",
-//   headers: {
-//     "Content-Type": "application/json",
-//     "api-key": data_api_key
-//   },
-//   body: ""
-// };
 
 if (!data_api_key) throw new Error('API Key not found');
 
@@ -27,77 +14,39 @@ const client = new MongoClient({
 const db = client.database("player_db");
 const players = db.collection("players");
 
-// const allActiveUsers = await players.find();
-// console.log(allActiveUsers);
+const addPlayer = async ({request, response}: { request: any; response: any;}) => {
+  try {
+    if (!request.hasBody) {
+      response.status = 400;
+      response.body = {
+        success: false,
+        msg: "No Data"
+      }
+    } else {
+      const body = await request.body();
+      const player = await body.value;
+      const insertedId = await players.insertOne({
+        _id: new ObjectId(),
+        data: player
+      });
 
-// const addPlayer = async ({
-//   request,
-//   response,
-// }: {
-//   request: any;
-//   response: any;
-// }) => {
-//   try {
-//     if (!request.hasBody) {
-//       response.status = 400;
-//       response.body = {
-//         success: false,
-//         msg: "No Data"
-//       };
-//     } else {
-//       const body = await request.body();
-//       const player = await body.value;
-//       const URI = `${BASE_URI}/insertOne`;
-//       const query = {
-//         collection: COLLECTION,
-//         database: DATABASE,
-//         dataSource: DATA_SOURCE,
-//         document: player
-//       };
-//       options.body = JSON.stringify(query);
-//       const dataResponse = await fetch(URI, options);
-//       const { insertedId } = await dataResponse.json();
-
-//       response.status = 201;
-//       response.body = {
-//         success: true,
-//         data: player,
-//         insertedId
-//       };
-//     }
-//   } catch (error) {
-//     response.body = {
-//       success: false,
-//       msg: error.toString(),
-//     }
-//   }
-// }
+      response.status = 201;
+      response.body = {
+        success: true,
+        data: player,
+        insertedId
+      }
+    }
+  } catch (error) {
+    response.body = {
+      success: false,
+      msg: error.toString(),
+    }
+  }
+}
 
 const getPlayers = async ({ response }: { response: any }) => {
   try {
-    // const URI = `${BASE_URI}/find`;
-    // const query = {
-    //   collection: COLLECTION,
-    //   database: DATABASE,
-    //   dataSource: DATA_SOURCE,
-    // };
-    // options.body = JSON.stringify(query);
-    // const dataResponse = await fetch(URI, options);
-    // const allPlayers = await dataResponse.json();
-
-    // if (allPlayers) {
-    //   response.status = 200;
-    //   response.body = {
-    //     success: true,
-    //     data: allPlayers
-    //   };
-    // } else {
-    //   response.status = 500;
-    //   response.body = {
-    //     success: false,
-    //     msg: "Internal Server Error"
-    //   };
-    // }
     const allPlayers = await players.find();
     if (allPlayers) {
       response.status = 200;
@@ -114,4 +63,4 @@ const getPlayers = async ({ response }: { response: any }) => {
   }
 };
 
-export { getPlayers };
+export { addPlayer, getPlayers };
--- routes.ts
+++ routes.ts
@@ -1,11 +1,11 @@
 import { Router } from "https://deno.land/x/oak/mod.ts";
-import { getPlayers } from "./controllers/players.ts";
+import { addPlayer, getPlayers } from "./controllers/players.ts";
 
 const router = new Router();
 
 // Implement routes
 router
-  // .post("/api/players", addPlayer) // Add a player
+  .post("/api/players", addPlayer) // Add a player
   .get("/api/players", getPlayers) // Get all players
 
 export default router;
